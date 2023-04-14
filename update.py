f4646e3b8f75cd24e847dcd240ad86ebabeb5e78
Jin Kwon <onacit@gmail.com>
2023-04-14 21:27:12 +0900

    wip

--- update.py
+++ update.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sqlite3
+URL = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService'
 try:
     connection = sqlite3.connect("./db/sc-oa-09-04.db")
 finally:
